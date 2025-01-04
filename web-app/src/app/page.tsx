0fe2d03cb000707251c55ffcb08f64ada4e16ec9
MattWong-ca <matthew.yt.wong@gmail.com>
2025-01-04 14:38:19 +0800

    Dark mode

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
           placeholder="Search projects..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="px-4 py-2 border rounded-md w-full md:w-64"
+          className="px-4 py-2 border rounded-md w-full md:w-64 text-black"
         />
         <select
           value={selectedEvent}
